059350736a6c9e8d3261a8a549ff147b84f81cd9
annalla <danghongxuyen2000@gmail.com>
2021-11-22 08:52:30 +0700

    "interface invite"

--- src/components/DetailedClass/PeopleItem/AddPeopleIcon.js
+++ src/components/DetailedClass/PeopleItem/AddPeopleIcon.js
@@ -4,22 +4,36 @@ import { Fragment } from "react";
 import PersonAddAltIcon from "@mui/icons-material/PersonAddAlt";
 import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
+import { EmailAutocomplete } from "./EmailAutocomplete";
+import Button from "@mui/material/Button";
 
 const style = {
   position: "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
+  width: 500,
   bgcolor: "background.paper",
-  border:"0px solid #000",
-  p: 4,
+  border: "0px solid #000",
+  p: 2,
+  minHeight: 400,
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
 };
-
-export default function AddPeopleIcon() {
+//code la linkTeacher
+export default function AddPeopleIcon({code}) {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const [emails, setEmails] = React.useState([]);
+  const constrolEmails = (email) => {
+    setEmails(email);
+  };
+  const handleInviteTeacher=()=>{
+    console.log(emails)
+    //emailJs
+  }
   return (
     <Fragment>
       <PersonAddAltIcon onClick={handleOpen} />
@@ -30,12 +44,38 @@ export default function AddPeopleIcon() {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+          <Box sx={{ width: "100%", maxHeight: "45vh" }}>
+            <Typography
+              sx={{ mb: "15px" }}
+              id="modal-modal-title"
+              variant="h6"
+              component="h2"
+            >
+              Invite teachers
+            </Typography>
+            <EmailAutocomplete control={constrolEmails} />
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row-reverse",
+              width: "100%",
+            }}
+          >
+            {emails.length === 0 ? (
+              <Button variant="outlined" sx={{ ml: "10px" }} disabled>
+                Invite
+              </Button>
+            ) : (
+              <Button variant="outlined" sx={{ ml: "10px" }} onClick={handleInviteTeacher}>
+                Invite
+              </Button>
+            )}
+
+            <Button variant="outlined" color="error" onClick={handleClose}>
+              Cancel
+            </Button>
+          </Box>
         </Box>
       </Modal>
     </Fragment>
