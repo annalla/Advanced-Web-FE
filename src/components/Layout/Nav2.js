5c880251eb314271c226067ed47599bc36959732
annalla <danghongxuyen2000@gmail.com>
2021-11-27 16:38:42 +0700

    "update nav2"

--- src/components/Layout/Nav2.js
+++ src/components/Layout/Nav2.js
@@ -1,53 +1,63 @@
-import React, { Fragment, useState, useEffect } from "react";
+import React, { Fragment, useState} from "react";
 import "./Nav2.css";
 import Divider from "@mui/material/Divider";
-import { FiSettings } from 'react-icons/fi';
-
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 import { AvatarIcon } from "./NavItems/Avatar";
 import { MenuDrawer } from "./NavItems/Menu";
 import { TabsItem } from "./NavItems/TabsItem";
 import { ClassSetting } from "../ClassSetting/ClassSetting";
+import Box from "@mui/material/Box";
 
 function Nav2({ data, valueTab }) {
-    const [isOpenSetting, setIsOpenSetting] = useState(false);
-    //useEffect(() => {
-    //    console.log(data)
-    //}, [data])
-    return (
-        <Fragment>
-            <header>
-                <div className="header2">
-                    <div className="part1">
-                        <div className="logo2">
-                            <span>
-                                <MenuDrawer />
-                            </span>
-                            <div className="blockName2">
-                                {data.name}
-                                {/* <span className="code">{data.code}</span> */}
-                            </div>
-                        </div>
-                        <div className="tabs">
-                            <TabsItem value={valueTab} isCustom={data.isCustom} />
-                        </div>
-                    </div>
-                    <div onClick={() => setIsOpenSetting(true)}>
-                    <FiSettings id="settingIcon" />
-                    </div>
-                    <nav>
-                        <ul>
-                            <li>
-                                <AvatarIcon />
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </header>
-            <Divider />
-            <div className="divide"></div>
-            {isOpenSetting ? <ClassSetting onclose={() => setIsOpenSetting(false)}  data={data} /> : ""}
-        </Fragment>
-    );
+  const [isOpenSetting, setIsOpenSetting] = useState(false);
+  //useEffect(() => {
+  //    console.log(data)
+  //}, [data])
+  return (
+    <Fragment>
+      <header>
+        <div className="header2">
+          <div className="part1">
+            <div className="logo2">
+              <span>
+                <MenuDrawer />
+              </span>
+              <div className="blockName2">
+                {data.name}
+                {/* <span className="code">{data.code}</span> */}
+              </div>
+            </div>
+            <div className="tabs">
+              <TabsItem value={valueTab} isCustom={data.isCustom} />
+            </div>
+          </div>
+
+          <nav>
+            <ul>
+              <li>
+                <Box sx={{py:"30%",height:"80%",alignItems:"center",mt:"30%"}}>
+                  {" "}
+                  <div onClick={() => setIsOpenSetting(true)}>
+                    <SettingsOutlinedIcon color="action" sx={{ fontSize: 25 }}/>
+                  </div>
+                </Box>
+              </li>
+              <li>
+                <AvatarIcon />
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <Divider />
+      <div className="divide"></div>
+      {isOpenSetting ? (
+        <ClassSetting onclose={() => setIsOpenSetting(false)} data={data} />
+      ) : (
+        ""
+      )}
+    </Fragment>
+  );
 }
 
 export { Nav2 };
